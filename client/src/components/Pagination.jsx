ffd1d8436f0bdab1f6dcd843d9c9fe333134b7cf
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-22 21:52:34 +0200

    Update Pagination.jsx

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,23 +1,29 @@
+import { FaChevronLeft, FaChevronRight, FaFastForward, FaFastBackward } from "react-icons/fa";
+
 export default function Pagination(){
     return (
-     <div className="flex justify-center gap-4 mt-8">
-     <button
-       className="bg-yellow-400 text-gray-900 px-4 py-2 rounded-full text-lg hover:bg-yellow-300"
+     <div className="flex justify-center items-center gap-4 mt-8">
+     <FaFastBackward className="cursor-pointer"></FaFastBackward>
+     <FaChevronLeft 
+     className="cursor-pointer"
     //    onClick={() => handlePageChange(currentPage - 1)}
     //    disabled={currentPage === 1}
        >
        Previous
-     </button>
+     </FaChevronLeft>
      <span className="text-lg text-gray-300">
        Page 1 of 3
      </span>
-     <button
-       className="bg-yellow-400 text-gray-900 px-4 py-2 rounded-full text-lg hover:bg-yellow-300"
+     <FaChevronRight
+     className="cursor-pointer"
     //    onClick={() => handlePageChange(currentPage + 1)}
     //    disabled={currentPage === totalPages}
        >
-       Next
-     </button>
+     </FaChevronRight>
+     <FaFastForward
+     className="cursor-pointer">
+
+     </FaFastForward>
    </div>
     )
 }
