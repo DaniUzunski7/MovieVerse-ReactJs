0cb134f1492532a36933cbf09886ddf6d65ad506
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-22 18:41:35 +0200

    Add routing and tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 
+import {Routes, Route} from 'react-router'
+
 function App() {
 
   return (
     <>
-      
+
+      <Routes>
+        <Route path="/" element={<h1>Works!</h1>} />
+        <Route path="/movies" element={<h1>Works!</h1>} />
+        <Route path="/tv-series" element={<h1>Works!</h1>} />
+        <Route path="/upcoming" element={<h1>Works!</h1>} />
+        <Route path="/highest-rated" element={<h1>Works!</h1>} />
+      </Routes>
     </>
   )
 }
