7f3ffca5e1a7475b5a7cdee0fb72e521fa773668
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-30 22:28:24 +0300

    Add register functionality

--- client/src/api/authAPI.js
+++ client/src/api/authAPI.js
@@ -3,13 +3,25 @@ import request from "../utils/request"
 const baseUrl =  'http://localhost:3030/users'
 
 export const useLogin = () => {
-    const login = async (email, password) => {
-       const data = await request.post(`${baseUrl}/login`, {email, password});
+    const login = (email, password) => {
+       const data = request.post(`${baseUrl}/login`, {email, password});
 
        return data;
     }
 
     return {
         login,
+    }
+}
+
+export const useRegister = () => {
+    const register = (email, userName, password) => {
+        const data = request.post(`${baseUrl}/register`, {email, userName, password});
+
+        return data;
+    }
+
+    return {
+        register
     }
 }
--- client/src/components/user/register/Register.jsx
+++ client/src/components/user/register/Register.jsx
@@ -1,23 +1,53 @@
+import { useNavigate } from 'react-router';
 import './register.css'
 
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { toast } from 'react-toastify';
+import { showErrorToast, showSuccessToast } from '../../toasts/ShowToast';
+import { useRegister } from '../../../api/authAPI';
+import { UserContext } from '../../../context/userContext';
 
 export default function Register(){
+    const navigate = useNavigate();
+
+    const {register} = useRegister();
+    const {loginHandler} = useContext(UserContext)
+  
     const [email, setEmail] = useState("");
     const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    
+
+    const registerHandler = async (formData) => {
+      const {email, userName, password, rePassword} = Object.fromEntries(formData)
+      
+      if(password !== rePassword){
+        showErrorToast('Passwords should match');
+        return
+      }
+
+      try {
+        const serverData = await register(email, userName, password);
+        
+        loginHandler(serverData)
+        
+        showSuccessToast('Register successfull');
+        navigate('/');
+      
+      } catch (error) {
+        showErrorToast(error);  
+      }
+    }
+
     return (
         <div className="register-container">
         <div className="form-container">
           <h2 className="form-title">Register</h2>
-          <form className="form">
+          <form className="form" action={registerHandler}>
             <div className="form-group">
               <label htmlFor="email">Email</label>
               <input
                 type="email"
                 id="email"
+                name='email'
                 placeholder="Enter your email"
                 className="form-input"
                 value={email}
@@ -25,14 +55,15 @@ export default function Register(){
               />
             </div>
             <div className="form-group">
-              <label htmlFor="email">User name</label>
+              <label htmlFor="userName">User name</label>
               <input
                 type="userName"
                 id="userName"
+                name="userName"
                 placeholder="Enter your user name"
                 className="form-input"
                 value={userName}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setUserName(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -40,21 +71,19 @@ export default function Register(){
               <input
                 type="password"
                 id="password"
+                name="password"
                 placeholder="Enter your password"
                 className="form-input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="form-group">
-              <label htmlFor="confirmPassword">Confirm Password</label>
+              <label htmlFor="rePassword">Confirm Password</label>
               <input
                 type="password"
-                id="confirmPassword"
+                id="rePassword"
+                name="rePassword"
                 placeholder="Confirm your password"
                 className="form-input"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
             <button type="submit" className="btn-submit">
