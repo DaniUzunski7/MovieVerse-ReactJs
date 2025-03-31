df02ea0dedd890a22d58a6700417eecc2055799b
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-31 23:56:27 +0300

    Add useAuth

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+
+export default function useAuth() {
+    const { accessToken } = useContext(UserContext);
+
+    
+    const options = {
+        headers: {
+            'X-Authorization': accessToken
+        }
+    }
+
+   return {
+    accessToken,
+    options
+   }
+}
