c26e53e85fc2f61616abfb61d046dd5c9d391b2e
Daniel Uzunski <dani.pleven@icloud.com>
2025-04-01 00:04:04 +0300

    Update movieServices only for upcoming movies

--- client/src/services/movieServices.js
+++ client/src/services/movieServices.js
@@ -1,35 +1,15 @@
 import request from "../utils/request"
 
 
-const baseUrl =  'http://localhost:3030/jsonstore'
+const baseUrl =  'http://localhost:3030/jsonstore/upcoming'
 
-function addMovie(movieData, dataPath){
-    return request.post(`${baseUrl}/${dataPath}`, movieData)
-}
 
-async function getAll(dataPath){
-    
-    const response = await request.get(`${baseUrl}/${dataPath}`);
+async function getAll(){
+    const response = await request.get(baseUrl);
     
     return Object.values(response)
 }
 
- function getOne(id, dataPath) {    
-    return request.get(`${baseUrl}/${dataPath}/${id}`);
-}
-
-function deleteMovie(dataPath, id) {
-    return request.delete(`${baseUrl}/${dataPath}/${id}`);
-}
-
-function editMovie(dataPath, id, movieData){
-    return request.put(`${baseUrl}/${dataPath}/${id}`, {...movieData, _id: id});
-}
-
 export default {
-    addMovie,
     getAll,
-    getOne,
-    deleteMovie,
-    editMovie
 }
