746909c7956f660cb49c55d9f395446e7b8a2709
Daniel Uzunski <dani.pleven@icloud.com>
2025-03-31 22:57:32 +0300

    Update Upcoming page

--- client/src/components/catalogs/Upcoming.jsx
+++ client/src/components/catalogs/Upcoming.jsx
@@ -1,34 +1,36 @@
 import { useEffect, useState } from "react";
+
 import movieServices from "../../services/movieServices";
-import MovieCard from "../MovieCard";
-import NoContent from "../NoContent";
-import { useParams } from "react-router";
 
 export default function Upcoming() {
   const [movies, setMovies] = useState([]);
-  const [error, setError] = useState();
 
   useEffect(() => {
-    movieServices.getAll("upcoming").then(setMovies).catch(setError);
+    movieServices.getAll("upcoming").then(setMovies);
   }, []);
 
-  const currentYear = new Date().getFullYear();
-
   return (
-    <div className="bg-gray-900 text-yellow-400 min-h-screen py-8 px-6">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        Upcoming Movies of {currentYear}
+    <div className="container mx-auto py-10 px-6">
+      <h1 className="text-4xl font-semibold text-yellow-400 text-center mb-6">
+        Upcoming Movies
       </h1>
-
-      {movies.length > 0 ? (
-        <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {movies.map((movie) => (
-            <MovieCard key={movie._id} {...movie} error={error} path="upcoming"/>
-          ))}
-        </div>
-      ) : (
-        <NoContent />
-      )}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+        {movies.map((movie) => (
+          <div key={movie._id} className="bg-gray-800 p-4 rounded-lg shadow-lg">
+            <img
+              src={movie.poster}
+              alt={movie.title}
+              className="w-full h-64 object-cover rounded-lg"
+            />
+            <h2 className="text-xl text-yellow-400 mt-3 font-semibold text-center">
+              {movie.title}
+            </h2>
+            <p className="text-gray-400 text-center mt-1">
+              Expected: {movie.year}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
