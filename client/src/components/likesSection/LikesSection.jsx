73adcaa74c19f2d4374343815e67f142241a3230
Daniel Uzunski <dani.pleven@icloud.com>
2025-04-02 22:04:47 +0300

    Update LikesSection.jsx

--- client/src/components/likesSection/LikesSection.jsx
+++ client/src/components/likesSection/LikesSection.jsx
@@ -1,26 +1,30 @@
-import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-export default function LikesSection() {
+export default function LikesSection({
+   isLiked,
+   onLike,
+   likesCount
+}) {   
+    
   return (
-    <>
-      <button
-        className={`relative top-7 mb-7 bg-yellow-400 text-gray-900 px-4 py-2 rounded-full transition-all duration-300 hover:bg-yellow-500 ${
-          "liked" ? "bg-yellow-500" : "bg-yellow-400"
-        }`}
-      >
-        <FaThumbsUp className="inline-block mr-2" />
-        {"liked" ? "Liked" : "Like"}
-        <span className={`relative ml-2`}>10</span>
-      </button>
-      <button
-        className={`relative top-7 mb-7 ml-2 bg-yellow-400 text-gray-900 px-4 py-2 rounded-full transition-all duration-300 hover:bg-yellow-500 ${
-          "disliked" ? "bg-yellow-500" : "bg-yellow-400"
-        }`}
-      >
-        <FaThumbsDown className="inline-block mr-2" />
-        {"liked" ? "Disliked" : "Dislike"}
-        <span className={`relative ml-2`}>10</span>
-      </button>
-    </>
+    <div className="flex justify-start mt-6">
+     <button
+      onClick={onLike}
+      disabled={isLiked}
+      className={`relative flex items-center gap-2 px-5 py-2 rounded-full font-medium text-gray-900 transition-all duration-300
+        ${isLiked ? "bg-yellow-500" : "bg-yellow-400 hover:bg-yellow-500"}
+        ${isLiked ? "scale-105" : "hover:scale-105"}
+        disabled:opacity-70 disabled:cursor-not-allowed`}
+    >
+      {isLiked ? (
+        <FaHeart className="text-2xl text-gray-900 transition-all duration-300" />
+      ) : (
+        <FaRegHeart className="text-2xl text-gray-900 transition-all duration-300" />
+      )}
+      <span className="text-gray-900 font-semibold">{likesCount}</span>
+    </button>
+  
+  </div>
+  
   );
 }
